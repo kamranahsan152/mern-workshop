a205676a60bdfd71de959cda0767cb7e1fe244a3
Kamran Ahsan <81046452+kamranahsan152@users.noreply.github.com>
2026-08-02 16:11:27 +0500

    notes model modified

--- server/models/Note.js
+++ server/models/Note.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const noteSchema = new mongoose.Schema({
   title: { type: String, required: true },
   body: { type: String },
+  done: { type: Boolean, default: false },
 });
 
 const Note = mongoose.model("Note", noteSchema);
